4d7a61bde4ef0adfbf7994ab577ee265ebadd2cf
hristo2612 <hristoapps@gmail.com>
2018-10-16 21:57:00 +0300

    Update files

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-require('./lib/record');
+#! /usr/bin/env node
+const path = require('path');
+const fs = require('fs');
+const args = process.argv;
+const argument = args[2];
+
+if (argument === "record") {
+    require('./lib/record');
+} else if (argument === "play") {
+    require('./lib/play');
+} else {
+    console.log('Please add an argument: record OR play');
+    console.log('Example: mkp record');
+    process.exit();
+}
